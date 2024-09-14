82e092357dd0a34b2c4071f0278a181c6a3937a2
kelvin <kelvin.oy16@gmail.com>
2024-09-14 20:01:58 +0000

    Homepage of music app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,169 @@
-import { View, Text, StyleSheet, Button, TextInput, FlatList } from 'react-native' // 
-import React, { useState } from 'react'
-import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
+import React from 'react'
 
-export default function ExploreScreen() {
-  const router = useRouter();
-  const [name, setName] = useState('');
 
-  const [fruits] = useState([
-    {id: '1', name: 'Apple'},
-    {id: '2', name: 'Orange'},
-    {id: '3', name: 'Banana'},
-    {id: '4', name: 'Grape'},
-    {id: '5', name: 'Guava'},
-  ])
-
+export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {name ? `Welcome, ${name}!` : 'Welcome to my first react native app!'}
-      </Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container} accessibilityLabel='container'>
+        <View style={styles.titleBar} accessibilityLabel='titleBar'>
+          <Text style={styles.titleBar_Title}>Home</Text>
+        </View>
 
-      <TextInput 
-      style={styles.input}
-      placeholder='Enter Your Name'
-      value={name}
-      onChangeText={setName}
-      />
+  {/* Top picks section with images */}
+        <View style={styles.topPicks}  accessibilityLabel='topPicks'>
+          <View >
+            <Text style={styles.topPicks_header}>Top Picks for You</Text>
 
-      <Text style={styles.name}>{`My name is: ${name}`}</Text>
-      
-      <Button 
-      title="Go to Explore"
-      onPress={() => router.push('/(tabs)/explore')}
-      />
+            <ScrollView contentContainerStyle={styles.scrollContent} horizontal={true} showsHorizontalScrollIndicator={false}>
+              <View style={styles.topPicks_img_container}>
 
-      <Button 
-      title="Go to Explore"
-      onPress={() => router.push('/(tabs)/explore')}
-      />
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/drake.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
 
-      <Button 
-      title="Go to Profile"
-      onPress={() => router.push('/(tabs)/profile')}
-      />
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/future.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
 
-      <FlatList
-      data={fruits}
-      keyExtractor={(item) => item.id}
-      renderItem={({item}) => (
-        <Text style={styles.listStyle}>{item.name}</Text>
-      )}
-      />
-    </View>
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/young_thug.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+
+        <View style={styles.explore}  accessibilityLabel='explore'>
+          <View >
+            <Text style={styles.topPicks_header}>Explore</Text>
+
+            <ScrollView contentContainerStyle={styles.scrollContent} horizontal={true} showsHorizontalScrollIndicator={false}>
+              <View style={styles.topPicks_img_container}>
+
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/drake.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
+
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/future.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
+
+                  <View accessibilityLabel='artistes' style={styles.imageContainer}>
+                      <Image
+                      source={require('../../assets/images/young_thug.jpg')}
+                      style={styles.img_artiste}
+                      />
+                  </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+
+        
+        
+      </View>
+    </ScrollView>
   )
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 50,
-    padding: 16,
-    backgroundColor: '#212121'
+    backgroundColor: "#000000",
+    height: "100%",
+    width: "100%",
+    color: "#ffffff"
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#ff4500',
-    backgroundColor: '#efefef',
-    padding: 20,
-    borderRadius: 5,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    width: '80%',
-    marginBottom: 20,
-    color: '#ffffff'
-  },
-  name: {
-    fontSize: 20,
-    color: 'green'
-  },
-  listStyle: {
-    color: '#ffffff',
-    fontSize: 18,
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    width: '100%',
-    textAlign: 'center',
-  }
   
-});
+  scrollContent: {
+    paddingBottom: 20, 
+  },
+
+  titleBar: {
+    width: "100%",
+    height: "10%",
+    marginTop: 30,
+    paddingLeft: 10
+  },
+
+  titleBar_Title: {
+    fontSize: 35,
+    color: "#ffffff",
+    marginTop: 40,
+    fontWeight: "bold"
+  },
+
+  topPicks: {
+    width: "100%",
+    height: "58%",
+    marginTop: 10,
+    padding: 15,
+  },
+
+  topPicks_header: {
+    fontSize: 23,
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    color: "#ffffff"
+
+  },
+
+  topPicks_img_container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 30,
+    gap: 20,
+  
+
+  
+  },
+
+  imageContainer: {
+    width: 320,
+    height: 400,
+    backgroundColor: "red",
+    borderRadius: 10
+  },
+
+  img_artiste: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    borderRadius: 10
+  },
+
+  explore: {
+    marginTop: -100,
+    width: "100%",
+    height: "58%",
+    padding: 15,
+  },
+
+  explore_header: {
+    fontSize: 23,
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    color: "#ffffff"
+  },
+
+  
+
+
+})
